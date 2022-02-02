924902c1c18debaf72cb84dac92d43b04e1b2802
faloird <funnyabird@gmail.com>
2022-02-02 14:23:24 +0100

    documentation FUN_FUN_00401eda

---
+++ Decompile functions/FUN_00401eda.c
@@ -0,0 +1,100 @@
+void FUN_00401eda(void)
+
+{
+  undefined4 *puVar1;
+  DWORD process_id;
+  HANDLE hProcess;
+  undefined4 extraout_ECX;
+  undefined4 uVar3;
+  undefined4 extraout_ECX_00;
+  undefined *unaff_ESI;
+  undefined *puVar4;
+  undefined *unaff_EDI;
+  undefined8 process;
+  undefined4 auStack556 [138];
+  
+  FUN_00401f6f();
+
+  puVar1 = (undefined4 *)&stack0xfffffffc;
+  
+  do {
+    
+    puVar4 = unaff_ESI;
+    unaff_ESI = (undefined *)puVar1;
+
+    /* Takes a snapshot of the specified processes, as well as the heaps, modules, and threads used by these processes.
+      [in] DWORD dwFlags = 2 = TH32CS_SNAPPROCESS (Includes all processes in the system in the snapshot) : The portions of the system to be included in the snapshot
+      [in] DWORD th32ProcessID = 0 (current process) : The process identifier of the process to be included in the snapshot.
+    
+    Return value : If the function succeeds, it returns an open handle to the specified snapshot.
+    */
+    HANDLE handle_snapshot = CreateToolhelp32Snapshot(2,0,unaff_EDI);
+
+    ADD_04D7FD54 = 0x22c; // Size of the PROCESSENTRY32W structure (0x22c = 556)
+
+    /* Retrieves information about the first process encountered in a system snapshot.
+        [in] HANDLE hSnapshot : A handle to the snapshot returned from a previous call to the CreateToolhelp32Snapshot function.
+        [in, out] LPPROCESSENTRY32W lppe : A pointer to a PROCESSENTRY32W structure. It contains process information such as the name of the executable file, the process identifier, and the process identifier of the parent process.
+
+        Return value : Returns TRUE if the first entry of the process list has been copied to the buffer or FALSE otherwise.
+    */
+    process = Process32FirstW(handle_snapshot, ADD_04D7FD54);
+
+    uVar3 = extraout_ECX;
+
+    while (process != 0) {
+      
+      longlong ret_val = FUN_00401fe8(uVar3,(uint)((ulonglong)process >> 0x20),ADD_04D7FD78);
+      
+      if (ret_val != 0) {
+        /* Retrieves the process identifier of the calling process.
+        Return value : The return value is the process identifier of the calling process.
+        */
+        process_id = GetCurrentProcessId();
+
+        if (process_id != *(DWORD *)ADD_04D6FD5C) {
+
+          /* Opens an existing local process object.
+            [in] DWORD dwDesiredAccess : The access to the process object.
+            [in] BOOL  bInheritHandle : If this value is TRUE, processes created by this process will inherit the handle. Otherwise, the processes do not inherit this handle.
+            [in] DWORD dwProcessId : The identifier of the local process to be opened.
+
+            Return value : If the function succeeds, the return value is an open handle to the specified process.
+          */
+          hProcess = OpenProcess(1,0,*(DWORD *)ADD_04D6FD5C);
+          
+
+          if (hProcess != (HANDLE)0x0) {
+            /* Terminates the specified process and all of its threads.
+              [in] HANDLE hProcess : A handle to the process to be terminated.
+              [in] UINT   uExitCode : The exit code to be used by the process and threads terminated as a result of this call.
+            */
+            TerminateProcess(hProcess,0);
+          }
+        }
+      }
+
+      /* Retrieves information about the next process recorded in a system snapshot.
+        [in]  HANDLE hSnapshot : A handle to the snapshot returned from a previous call to the CreateToolhelp32Snapshot function.
+        [out] LPPROCESSENTRY32W lppe : A pointer to a PROCESSENTRY32W structure. It contains process information such as the name of the executable file, the process identifier, and the process identifier of the parent process.
+
+        Return value : Returns TRUE if the next entry of the process list has been copied to the buffer or FALSE otherwise.
+      */
+      process = Process32NextW(handle_snapshot,ADD_04D7FD54);
+      uVar3 = extraout_ECX_00;
+    }
+
+    /* Closes an open object handle.
+      [in] HANDLE hObject : A valid handle to an open object.
+    */
+    CloseHandle(handle_snapshot);
+
+    auStack556[0] = 1000;
+    
+    FUN_0040212a();
+    
+    puVar1 = auStack556;
+    unaff_EDI = puVar4;
+
+  } while( true );
+}
--- Decompile functions/FUN_00401f9e.c
+++ Decompile functions/FUN_00401f9e.c
@@ -6,12 +6,12 @@
                              undefined __cdecl FUN_00401f9e(PLUID param_1, HANDLE par
              undefined         AL:1           <RETURN>
              PLUID             Stack[0x4]:4   param_1
-             HANDLE            Stack[0x8]:4   param_2                                 XREF[1]:     00401fdc(R)  
+             HANDLE            Stack[0x8]:4   hAccessToken                                 XREF[1]:     00401fdc(R)  
              undefined4        Stack[0xc]:4   param_3
              DWORD             Stack[0x10]:4  param_4                                 XREF[1]:     00401fb1(R)  
              LONG              Stack[0x14]:4  param_5                                 XREF[1]:     00401fb9(R)  
              undefined4        Stack[0x18]:4  param_6                                 XREF[1]:     00401fc9(*)  
-             DWORD             Stack[0x1c]:4  param_7                                 XREF[3]:     00401fa9(W), 
+             DWORD             Stack[0x1c]:4  tokenPrivStruct                                 XREF[3]:     00401fa9(W), 
                                                                                                    00401fce(*), 
                                                                                                    00401fd5(*)  
              DWORD             Stack[0x20]:4  param_8                                 XREF[1]:     00401fb5(W)  
@@ -24,7 +24,7 @@
                  00 00
         00401fa5 85 c0           TEST       EAX,EAX
         00401fa7 74 3d           JZ         LAB_00401fe6
-        00401fa9 c7 44 24        MOV        dword ptr [ESP + param_7],0x1
+        00401fa9 c7 44 24        MOV        dword ptr [ESP + tokenPrivStruct],0x1
                  14 01 00 
                  00 00
         00401fb1 8b 44 24 08     MOV        EAX,dword ptr [ESP + param_4]
@@ -36,13 +36,13 @@
                  00 00
         00401fc9 8d 44 24 10     LEA        EAX=>param_6,[ESP + 0x10]
         00401fcd 50              PUSH       EAX
-        00401fce 8d 44 24 18     LEA        EAX=>param_7,[ESP + 0x18]
+        00401fce 8d 44 24 18     LEA        EAX=>tokenPrivStruct,[ESP + 0x18]
         00401fd2 50              PUSH       EAX
         00401fd3 6a 10           PUSH       0x10
-        00401fd5 8d 44 24 20     LEA        EAX=>param_7,[ESP + 0x20]
+        00401fd5 8d 44 24 20     LEA        EAX=>tokenPrivStruct,[ESP + 0x20]
         00401fd9 50              PUSH       EAX
         00401fda 6a 00           PUSH       0x0
-        00401fdc 8b 44 24 14     MOV        EAX,dword ptr [ESP + param_2]
+        00401fdc 8b 44 24 14     MOV        EAX,dword ptr [ESP + hAccessToken]
         00401fe0 50              PUSH       EAX
         00401fe1 e8 8c 01        CALL       AdjustTokenPrivileges                            BOOL AdjustTokenPrivileges(HANDL
                  00 00
@@ -54,34 +54,49 @@
 /*
 * Fonction sans retour dont le but semble de vérifier si le programme détient les bon privilèges Windows lié à son "token"
 * Dans un premier temps, la fonction vérifie les privilèges du token courant via utilisation de la librairie ADVAPI32.DLL.
-* Si le token ne possède pas les bons privilèges, alors la fonction fait appel de nouveau a la librairie ADVAPI32.DLL afin de mettre à jour les privilèges de son token
+* Si le token ne possède pas des bons privilèges, alors la fonction fait appel de nouveau a la librairie ADVAPI32.DLL afin de mettre à jour les privilèges de son token
 * 
 * Paramètres: information sytèmes lié au processus du malware
 *
 * Retourne: void
 */
 void __cdecl
-FUN_00401f9e(PLUID param_1,HANDLE param_2,undefined4 param_3,DWORD param_4,LONG param_5,
-            DWORD param_6,DWORD param_7,DWORD param_8,LONG param_9,DWORD param_10)
+FUN_00401f9e(PLUID param_1,HANDLE hAccessToken,undefined4 param_3,DWORD param_4,LONG param_5,
+            DWORD param_6,DWORD tokenPrivStruct,DWORD param_8,LONG param_9,DWORD param_10)
 
 {
-  BOOL BVar1; // Création d'un booléen
-  LPCSTR unaff_retaddr; // Création d'un pointeur vers une chaîne de caractères terminée.
+  BOOL ret_value; 
+  LPCSTR unaff_retaddr;
   
-  BVar1 = LookupPrivilegeValueA((LPCSTR)0x0,unaff_retaddr,param_1); // Appel à la fonction LookupPrivilegeValueA (ADVAPI32.DLL) pour vérifier le token de privilèges
+  /* The LookupPrivilegeValue function retrieves the locally unique identifier (LUID) used on a specified system to locally represent the specified privilege name.
+    [in, optional] LPCSTR lpSystemName : If a null string is specified, the function attempts to find the privilege name on the local system.
+    [in] LPCSTR lpName : A pointer to a null-terminated string that specifies the name of the privilege, as defined in the Winnt.h header file.
+    [out] PLUID  lpLuid : A pointer to a variable that receives the LUID by which the privilege is known on the system specified by the lpSystemName parameter.
+  
+  Return value : If the function succeeds, the function returns nonzero.
+  */
+  ret_value = LookupPrivilegeValueA((LPCSTR)0x0,unaff_retaddr,param_1); // Appel à la fonction LookupPrivilegeValueA (ADVAPI32.DLL) pour vérifier le token de privilèges
   
   // Si notre booléen est faux après execution de la précédente fonction (erreur dans l'accès aux privilèges du token)
-  if (BVar1 != 0) {
+  if (ret_value != 0) {
     // Changement de valeur des paramètres suivants
-    param_7 = 1;
+    tokenPrivStruct = 1;
     param_8 = param_4;
     param_9 = param_5;
     param_10 = 2;
     
     // Appel à la fonction AdjustTokenPrivileges (ADVAPI32.DLL) avec les nouveaux paramètres pour mettre à jour le token de privilèges.
-    AdjustTokenPrivileges(param_2,0,(PTOKEN_PRIVILEGES)&param_7,0x10,(PTOKEN_PRIVILEGES)&param_7,&param_6);
+    /* The AdjustTokenPrivileges function enables or disables privileges in the specified access token.
+      [in] HANDLE TokenHandle : A handle to the access token that contains the privileges to be modified. 
+      [in] BOOL DisableAllPrivileges : Specifies whether the function disables all of the token's privileges. If it is FALSE, the function modifies privileges based on the information pointed to by the NewState parameter.
+      [in, optional]  PTOKEN_PRIVILEGES NewState : A pointer to a TOKEN_PRIVILEGES structure that specifies an array of privileges and their attributes.
+      [in] DWORD BufferLength : Specifies the size, in bytes, of the buffer pointed to by the PreviousState parameter.
+      [out, optional] PTOKEN_PRIVILEGES PreviousState : A pointer to a buffer that the function fills with a TOKEN_PRIVILEGES structure that contains the previous state of any privileges that the function modifies.
+      [out, optional] PDWORD ReturnLength : A pointer to a variable that receives the required size, in bytes, of the buffer pointed to by the PreviousState parameter
+    */
+    AdjustTokenPrivileges(hAccessToken,0,(PTOKEN_PRIVILEGES)&tokenPrivStruct,16,(PTOKEN_PRIVILEGES)&tokenPrivStruct,&param_6);
   }
-  return; // On ne retourne rien car de type void
+  return;
 }
 
 /*
