a3214923b3f29f42850beaed974dc5ad893c2ef6
faloird <funnyabird@gmail.com>
2022-02-02 22:20:25 +0100

    Add all functions

---
+++ Decompile functions/FUN_0040204c.c
@@ -0,0 +1,9 @@
+
+void FUN_0040204c(void)
+
+{
+                    /* WARNING: Treating indirect jump as call */
+  SetTimer();
+  return;
+}
+
---
+++ Decompile functions/LAB_004010cf.c
@@ -0,0 +1,9 @@
+
+void UndefinedFunction_004010cf(void)
+
+{
+  s__files_files/s_s_ftreads_network_00404000[81] = 'y';
+  FUN_004010e7((LPNETRESOURCEA)0x0,1);
+  s__files_files/s_s_ftreads_network_00404000[81] = 'n';
+  return;
+}
